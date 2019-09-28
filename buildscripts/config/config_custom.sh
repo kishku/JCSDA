a4a6e642683c35e8b731316b649d46c38e4b1d75
Yannick Trémolet <tremolet@ucar.edu>
2019-09-28 09:17:39 -0600

    Custom build without modules

--- buildscripts/config/config_custom.sh
+++ buildscripts/config/config_custom.sh
@@ -8,8 +8,9 @@
 #export COMPILER="intel/17.0.1"
 #export MPI="impi/17.0.1"
 
-export COMPILER="clang/6.0.1"
-export MPI="openmpi/3.1.2"
+export MODULES=false
+export COMPILER="clang/9.1.0"
+export MPI="mpich/3.3.1"
 
 # This tells jedi-stack how you want to build the compiler and mpi modules
 # valid options include:
@@ -19,20 +20,24 @@ export MPI="openmpi/3.1.2"
 #             This is a common option for, e.g., gcc/g++/gfortrant
 # from-source: This is to build from source
 export COMPILER_BUILD="native-pkg"
-export MPI_BUILD="from-source"
+export MPI_BUILD="native-pkg"
+
+# For nccmp. This magically fixes the make install command.
+export MKDIR_P="mkdir -p"
 
 # Build options
 export PREFIX=/opt/modules 
 export USE_SUDO=Y
 export PKGDIR=pkg
 export LOGDIR=buildscripts/log
-export OVERWRITE=Y
+export OVERWRITE=N
 export NTHREADS=4
-export   MAKE_CHECK=N
+export MAKE_CHECK=N
 export MAKE_VERBOSE=Y
-export   MAKE_CLEAN=N
+export MAKE_CLEAN=N
 export DOWNLOAD_ONLY=N
 export STACK_EXIT_ON_FAIL=Y
+export WGET="wget -nv"
 
 # Minimal JEDI Stack
 export      STACK_BUILD_CMAKE=N
@@ -42,12 +47,12 @@ export       STACK_BUILD_ZLIB=N
 export     STACK_BUILD_LAPACK=N
 export    STACK_BOOST_HEADERS=N
 export     STACK_BUILD_EIGEN3=N
-export       STACK_BUILD_HDF5=N
-export    STACK_BUILD_PNETCDF=N
-export     STACK_BUILD_NETCDF=N
-export      STACK_BUILD_NCCMP=N
+export       STACK_BUILD_HDF5=Y
+export    STACK_BUILD_PNETCDF=Y
+export     STACK_BUILD_NETCDF=Y
+export      STACK_BUILD_NCCMP=Y
 export        STACK_BUILD_NCO=N
-export    STACK_BUILD_ECBUILD=Y
+export    STACK_BUILD_ECBUILD=N
 export      STACK_BUILD_ECKIT=N
 export      STACK_BUILD_FCKIT=N
 export        STACK_BUILD_ODB=N
@@ -64,4 +69,6 @@ export        STACK_BUILD_TKDIFF=N
 export          STACK_BOOST_FULL=N
 export          STACK_BUILD_ESMF=N
 export      STACK_BUILD_BASELIBS=N
+export     STACK_BUILD_PDTOOLKIT=N
+export          STACK_BUILD_TAU2=N
 
