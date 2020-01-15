fc170cda91a7af39775f4aecd462072c4eb5b0e8
Mark Miesch <miesch@ucar.edu>
2020-01-15 12:13:08 -0700

    remove python 2.7 support

--- buildscripts/libs/build_pyjedi.sh
+++ buildscripts/libs/build_pyjedi.sh
@@ -13,10 +13,6 @@ name="pyjedi"
 # Python Package installs
 #####################################################################
 
-$SUDO python -m pip install -U pip setuptools
-$SUDO python -m pip install -U numpy
-$SUDO python -m pip install -U wheel netCDF4 matplotlib
-
 $SUDO python3 -m pip install -U pip setuptools
 $SUDO python3 -m pip install -U numpy
 $SUDO python3 -m pip install -U wheel netCDF4 matplotlib
@@ -34,9 +30,6 @@ $SUDO python3 -m pip install -U pytest
 cd ${JEDI_STACK_ROOT}/${PKGDIR:-"pkg"}
 git clone https://github.com/JCSDA/py-ncepbufr.git 
 cd py-ncepbufr 
-
-CC=gcc python setup.py build 
-$SUDO python setup.py install 
 
 CC=gcc python3 setup.py build 
 $SUDO python3 setup.py install 
