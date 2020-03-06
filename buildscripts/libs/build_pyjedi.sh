1d22eae9101122a2e7deec2ac3a2c1f7818f52df
Jin Lee <jin.lee@bom.gov.au>
2020-03-06 20:31:35 +0000

    Changes to handle Gadi's barebones Python modulefiles; generalised script to allow installation of ncepbufr module in user's .local site-packages

--- buildscripts/libs/build_pyjedi.sh
+++ buildscripts/libs/build_pyjedi.sh
@@ -13,9 +13,17 @@ name="pyjedi"
 # Python Package installs
 #####################################################################
 
+# force the use of Python2 before installing Python2 packages
+module unload python2 python3
+module load python2/2.7.17
+
 $SUDO python -m pip install -U pip setuptools
 $SUDO python -m pip install -U numpy
 $SUDO python -m pip install -U wheel netCDF4 matplotlib
+
+# force the use of Python3 before installing Python3 packages
+module unload python2 python3
+module load python3/3.7.4
 
 $SUDO python3 -m pip install -U pip setuptools
 $SUDO python3 -m pip install -U numpy
@@ -35,10 +43,34 @@ cd ${JEDI_STACK_ROOT}/${PKGDIR:-"pkg"}
 git clone https://github.com/JCSDA/py-ncepbufr.git 
 cd py-ncepbufr 
 
+# force the use of Python2 before installing Python2 packages
+module unload python2 python3
+module load python2/2.7.17
+
 CC=gcc python setup.py build 
-$SUDO python setup.py install 
+if [[ $USE_SUDO =~ [yYtT] ]]
+then
+    $SUDO python setup.py install
+else
+    python setup.py install --user
+fi
+
+# force the use of Python3 before installing Python3 packages
+module unload python2 python3
+module load python3/3.7.4
+
+# Python module search path seems to have "$HOME/.local/lib/python2.7" first and
+# so during the command, "python3 setup.py build" Python2 modules are picked up
+# first. To prevent this and to force the command to use Python3 following is
+# added
+export PYTHONPATH=${HOME}/.local/lib/python3.7/site-packages
 
 CC=gcc python3 setup.py build 
-$SUDO python3 setup.py install 
+if [[ $USE_SUDO =~ [yYtT] ]]
+then
+    $SUDO python3 setup.py install
+else
+    python3 setup.py install --user
+fi
 
 exit 0
