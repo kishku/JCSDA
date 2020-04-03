0c0343762b032c3528824ff83d1f7b0a0dfd0d4e
mmiesch <miesch@ucar.edu>
2020-04-02 20:35:06 -0600

    develop branch of jedi-stack

--- buildscripts/build_stack.sh
+++ buildscripts/build_stack.sh
@@ -88,7 +88,7 @@ build_lib LAPACK lapack 3.7.0
 build_lib BOOST_HDRS boost 1.68.0 headers-only
 build_lib EIGEN3 eigen 3.3.5
 build_lib BUFRLIB bufrlib master
-build_lib ECBUILD ecbuild jcsda bugfix/intel-shlib-undefined
+build_lib ECBUILD ecbuild jcsda develop
 
 #----------------------
 # These must be rebuilt for each MPI implementation
